d8cd26a9cfdddbb72ce884ca58e16cfcdccf3a67
pedraal <pro@pedraal.fr>
2024-09-19 01:05:14 +0200

    fix: auto install for tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
 			ensure_installed = { "lua", "gleam", "javascript" },
+			auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
