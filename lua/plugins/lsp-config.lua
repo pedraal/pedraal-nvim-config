53bb788d6954c1bb62413e707ec58ecb1ea936c1
pedraal <pro@pedraal.fr>
2024-09-19 09:47:38 +0200

    feat: vue lsp setup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,43 +1,52 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gleam.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.biome.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "ga", vim.lsp.buf.code_action, {})
-		end,
-	},
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gleam.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.biome.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "javascript", "vue" },
+        init_options = {
+          vue = {
+            hybridMode = false,
+          },
+        },
+      })
+      vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "ga", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
