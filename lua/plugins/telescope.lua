cd95396945bfa14a28004724c943e108bd3b9143
pedraal <pro@pedraal.fr>
2024-09-28 02:15:07 +0200

    feat: update telescope keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
 		end,
 	},
 	{
