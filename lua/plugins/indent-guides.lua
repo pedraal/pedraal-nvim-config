3d4938fbf6f82e2f1830e7857066269540559d9f
pedraal <pro@pedraal.fr>
2024-09-28 02:16:37 +0200

    feat: better comments and add indentation guides

---
+++ lua/plugins/indent-guides.lua
@@ -0,0 +1,31 @@
+return { -- indent guides for Neovim
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = {
+				enabled = true,
+			},
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
+		main = "ibl",
+	},
+}
